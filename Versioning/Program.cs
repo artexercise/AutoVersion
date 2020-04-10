2ca866d975b6608132d747176b8325eb6425d319
Joseph Jeskiewicz <artexercise@yahoo.com>
2020-04-10 17:45:33 -0400

    Initial Commit

---
+++ Versioning/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * This Question:
+             * https://stackoverflow.com/questions/826777/how-to-have-an-auto-incrementing-version-number-visual-studio
+             * 
+             * Leads to this:
+             * https://docs.microsoft.com/en-us/visualstudio/modeling/writing-a-t4-text-template?view=vs-2019
+             * 
+             * Interesting!
+             * So this project is an example of automatic versioning.
+             */
+            System.Diagnostics.Debug.WriteLine("Version Testing.");
+            Console.WriteLine("Version Testing.\n\r");
+
+            System.Diagnostics.Debug.WriteLine("The version of the currently executing assembly is: {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+            Console.WriteLine("The version of the currently executing assembly is: {0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Versioning/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Versioning")]
+[assembly: AssemblyDescription("Automatic Version Changing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Some Company")]
+[assembly: AssemblyProduct("Versioning")]
+[assembly: AssemblyCopyright("Copyright © Some Company 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ced910d0-91a8-49e4-9cce-4ccbf7e418e0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
